d22b15c56c52691989f2327ad3c53efe3a712a70
Joao Piologo <joaotpiologo@hotmail.com>
2025-01-15 20:29:37 -0300

    Added book search

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../images/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript', id: 2, src:livro},
+    {nome: 'Cangaceiro', id: 3, src:livro},
+    {nome: 'Star Wars da Programação', id: 4, src:livro},
+]
